5d3d75c0cf9729f590841fc72ec8ea0744cb6366
Otakoe <vitalylarc@gmail.com>
2020-03-03 01:12:57 +0200

    Решил последнее задание из первого дз

---
+++ src/Task1Dop2/Dop2.java
@@ -0,0 +1,10 @@
+package Task1Dop2;
+
+import java.util.Scanner;
+
+public class Dop2 {
+    Scanner sc = new Scanner (System.in);
+    int a,b;
+    System.out.println("Введите два целых числа через пробел или энтер");
+
+}
